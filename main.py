7703e30fb340e644f1f3b69f959c78e90e2b73f3
Martin Valletta <martin.valleta@55social.com>
2020-07-30 20:58:53 -0300

    Add register client and query public ip endpoints

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+robots = {}
 
 @app.route('/')
 def index():
     """Return a friendly HTTP greeting."""
     return 'Welcome to Manager initial page!'
 
+@app.route('/robot/<robot_id>/address')
+def get_robot_address(robot_id):
+    """Gets the public IP used to connect to the robot."""
+    return robots[robot_id]
+
+@app.route('/register/<robot_id>', methods = ['POST'])
+def register(robot_id):
+    """Register the public IP of the robot."""
+    robots[robot_id] = request.remote_addr
+    return 'OK'
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='192.168.0.6', port=8080, debug=True)
