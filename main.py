94894c2a5228951d326acca44b2689cdd6ac89a9
Martin Valletta <martin.valleta@55social.com>
2020-09-22 22:28:51 -0300

    Websockets using Flask first version

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
+from falsk_sockets import Sockets
 
 app = Flask(__name__)
+sockets = Sockets(app)
 
 robots = {}
 
@@ -17,8 +19,20 @@ def get_robot_address(robot_id):
 @app.route('/register/<robot_id>', methods = ['POST'])
 def register(robot_id):
     """Register the public IP of the robot."""
-    robots[robot_id] = request.remote_addr
+    if request.headers.getlist("X-Forwarded-For"):
+    	ip = request.headers.getlist("X-Forwarded-For")[0]
+    else:
+    	ip = request.remote_addr
+    robots[robot_id] = ip
     return 'OK'
+
+@app.route('/control')
+def control(ws):
+	while not ws.closed:
+		message = ws.receive()
+		if message is None:
+			continue
+		print(message)
 
 if __name__ == '__main__':
     app.run(host='192.168.0.6', port=8080, debug=True)
